587f73c201e0199bf56eb627b96c876595b2e7d9
JBalzano <jillbalzano@apress.com>
2021-12-08 16:17:11 -0500

    Adding code

---
+++ MessageContracts/MessageContracts.cs
@@ -0,0 +1,25 @@
+﻿namespace MessageContracts
+{
+    public interface IInvoiceCreated
+    {
+        int InvoiceNumber { get; }
+        IInvoiceToCreate InvoiceData { get; }
+    }
+
+    public interface IInvoiceToCreate
+    {
+        int CustomerNumber { get; set; }
+        List<InvoiceItems> InvoiceItems { get; set; }
+    }
+
+    public class InvoiceItems
+    {
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public double ActualMileage { get; set; }
+        public double BaseRate { get; set; }
+        public bool IsOversized { get; set; }
+        public bool IsRefrigerated { get; set; }
+        public bool IsHazardousMaterial { get; set; }
+    }
+}
